af8ef4f9f2120fdb7da89c7b8d0562b5dcef3882
Fiterman <gfiterman96@gmail.com>
2023-10-06 01:22:30 -0300

    style: update color palette feat: import boot supabase and linting init supabase Install Supabase

---
+++ src/boot/supabase.js
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_BASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLIC;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("Init Supabase:\n", supabase);
+
+const useSupabase = () => {
+  return supabase;
+};
