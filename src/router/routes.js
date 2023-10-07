c62a3efef62e0fe99142e83e6ae265303387e589
Fiterman <gfiterman96@gmail.com>
2023-10-07 01:06:39 -0300

    feat: add login route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,39 @@
 const routes = [
     {
         path: "/",
-        component: () => import("layouts/MainLayout.vue"),
+        component: () => import("../layouts/LoginLayout.vue"),
         children: [
-            { path: "", component: () => import("pages/IndexPage.vue") },
+            {
+                path: "",
+                name: "loginDefault",
+                component: () => import("pages/Login.vue"),
+            },
+            {
+                path: "login",
+                name: "login",
+                component: () => import("pages/Login.vue"),
+            },
+            {
+                path: "register",
+                name: "register",
+                component: () => import("pages/Register.vue"),
+            },
         ],
+    },
+    {
+        path: "/",
+        component: () => import("layouts/MainLayout.vue"),
+        children: [{ path: "/", component: () => import("pages/Index.vue") }],
+        meta: {
+            requiresAuth: true,
+        },
     },
 
     // Always leave this as last one,
     // but you can also remove it
     {
         path: "/:catchAll(.*)*",
-        component: () => import("pages/ErrorNotFound.vue"),
+        component: () => import("pages/Error404.vue"),
     },
 ];
 
